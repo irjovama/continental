cba8d933ae819605ac8eea4aa7b54b51dcf3cf0b
IRVING JONES VALDES MACIEL <irjovama@gmail.com>
2023-04-11 13:44:52 -0600

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,22 +6,11 @@ Rails.application.routes.draw do
 
   get '/api/v1/report', to: 'report#index'
 
-  get '/api/v1/users', to: 'user#index'
-  get '/api/v1/users/:id', to: 'user#show'
-  post '/api/v1/users', to: 'user#create'
-  put '/api/v1/users/:id', to: 'user#update'
-  delete '/api/v1/users/:id', to: 'user#destroy'
-
-  # get '/api/v1/tests', to: 'tests#index'
-  # get '/api/v1/tests/:id', to: 'tests#show'
-  # post '/api/v1/tests', to: 'tests#create'
-  # put '/api/v1/tests/:id', to: 'tests#update'
-  # delete '/api/v1/tests/:id', to: 'tests#destroy'
-
-
-  get '/api/v1/users/:user_id/tests', to: 'user_tests#index'
-  get '/api/v1/users/:user_id/tests/:test_id', to: 'user_tests#show'
-  resources :tests, only: [:index, :show, :destroy, :update, :create], path '/api/v1/tests'
+  # get '/api/v1/users/:user_id/tests', to: 'user_tests#index'
+  # get '/api/v1/users/:user_id/tests/:test_id', to: 'user_tests#show'
+  
+  resources :tests, only: [:index, :show, :destroy, :update, :create], path: '/api/v1/tests'
+  resources :user, only: [:index, :show, :destroy, :update, :create], path: '/api/v1/users'
 
   resources :categories, only: [:index, :show, :destroy, :update, :create], path: '/api/v1/categories'
   resources :questions, only: [:index, :show, :destroy, :update, :create], path: '/api/v1/questions'
