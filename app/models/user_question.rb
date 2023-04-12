4852572d31d2bd614312b79616cbdcf5b160633d
Angelo C <angmogollon@gmail.com>
2023-04-12 17:19:35 -0300

    Adding validation to UserQuestions

--- app/models/user_question.rb
+++ app/models/user_question.rb
@@ -1,6 +1,20 @@
+class UserQuestionValidator < ActiveModel::Validator
+  def validate(record)
+    if record.question.question_type != "points" && record.evaluation.to_i > 0
+      record.errors.add :base, "This is not a points type of question"
+    end
+  end
+end
+
+
 class UserQuestion < ApplicationRecord
   validates :user_id, :question_id, :evaluation, presence: :true
+  validates_with UserQuestionValidator
 
   belongs_to :user
   belongs_to :question
 end
+
+
+
+
